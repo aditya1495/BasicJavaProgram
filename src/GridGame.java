de81286ac14dc2aacb15daec9cad4685dff8721d
aditya1495 <adityashah1495@gmail.com>
2016-05-18 15:52:45 +0530

    Added health of player.

--- src/GridGame.java
+++ src/GridGame.java
@@ -5,21 +5,22 @@ public class GridGame {
     private void play() {
         Grid myGrid = new Grid(5);
         Scanner sc = new Scanner(System.in);
-        while (!myGrid.hasReached()) {
+        boolean quitGame = false;
+        while (!quitGame) {
             char c = sc.next().charAt(0);
             if (c == 'U') {
-                myGrid.moveUp();
+                quitGame = myGrid.moveUp();
             } else if (c == 'L') {
-                myGrid.moveLeft();
+                quitGame = myGrid.moveLeft();
             } else if (c == 'R') {
-                myGrid.moveRight();
-            } else if (c == 'D'){
-                myGrid.moveDown();
+                quitGame = myGrid.moveRight();
+            } else if (c == 'D') {
+                quitGame = myGrid.moveDown();
             } else {
                 System.out.println("Invalid Action Demanded!");
             }
         }
-        System.out.println("Game over!");
+        System.out.println("Game finished!");
     }
 
     public static void main(String[] args) {
